bf90aa8b463a9cbb30b07d6b0bfdd737d00e2de1
ortariot <ortariot@gmail.com>
2021-09-24 15:50:11 +0700

    create project

---
+++ articles/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from articles.models import Article
+
+
+def articles_list(request):
+    template = 'articles/news.html'
+    article_query = Article.objects.all()
+
+    context = {'object_list': article_query}
+
+    
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = '-published_at'
+
+    return render(request, template, context)
